4a73b3aab153ba84affc85ec339cc6eb38f4ee2e
atdaniellimon <d4nilin0n@icloud.com>
2026-07-21 23:12:53 -0600

    Delete arch/i386 directory

--- arch/i386/io.h
+++
@@ -1,9 +0,0 @@
-#ifndef IO_H
-#define IO_H
-
-unsigned char inb(unsigned short port);
-unsigned short inw(unsigned short port);
-void outb(unsigned short port, unsigned char val);
-void outw(unsigned short port, unsigned short val);
-
-#endif
